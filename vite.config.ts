026c0d3c58079f62a5ae324015eaf3760234f4d6
Alexsander <alexsander.santos.contato@gmail.com>
2026-03-17 13:54:17 -0300

    Feito configuração para o github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/leaozinho-prototipo-app/', // Coloque o nome exato do seu repositório aqui
 })
